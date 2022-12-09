ec50ce6977cc1704c99fa4028b082e599cc98d6c
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-12-09 10:01:40 +0000

    c

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 #ejercicio 1
 jugs=("BANANA") #consonates
 jugk=("BANANA") #vocales
+palabra=("BANANA")
+
 
 def minion_game(string):
   consonantes=""
   vocales= ""
-  
+  pos=0
+  while(letra)
+    
 
 
   
